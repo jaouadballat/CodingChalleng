e77357aaa6397cdf233fc81266e17055cea424fe
jaouad <jaouad@nextmedia.ma>
2019-01-16 21:55:18 +0100

    loading more data from api

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,32 +9,65 @@ class Home extends Component {
     super(props);
     this.state = {
       repositories: [],
-      laoding: false
+      page:0,
+      laoding: false,
+      loadingMore: false,
+      url
     }
   }
 
   componentDidMount() {
-    this.fetchApi();
+    this.fetchApi(this.state.url);
+    this.fetchMoreData();
   }
 
-  fetchApi = () => {
+  fetchApi = (url) => {
     this.setState({loading: true})
     axios.get(url)
-      .then(({ data }) => this.setState({repositories: data.items, loading: false}))
-      .catch(error => console.log(error.response.data))
+      .then((response) => this.setState({repositories: response.data.items, loading: false}))
+      .catch(error => {
+        console.log(error.response)
+        if(error.response.status === 403) return;
+      })
+  }
+
+  fetchMoreData = () => {
+    document.addEventListener('scroll',  (event) => {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        this.incrementPage();
+        this.setState({loadingMore: true})
+        let url = `${this.state.url}&page=${this.state.page}`;
+        axios.get(url)
+          .then(({data}) => {
+            let items = [...this.state.repositories, ...data.items];
+            this.setState({repositories: items, loadingMore: false})
+          })
+          .catch(error => {
+            console.log(error)
+          });
+    }
+  });
+  }
+
+  incrementPage = () => {
+    this.setState(function (prevState) {
+      return {
+        page: prevState.page + 1
+      };
+    });
   }
 
 
 
   render() {
-    const { repositories, loading } = this.state;
-    console.log(repositories)
+    const { repositories, loading, loadingMore } = this.state;
     return (
       <Fragment>
         {
           loading ? <div>loading....</div>
           : <Lists repositories={repositories} />
         }
+        {loadingMore && <div>Loading more ....</div>}
       </Fragment>
     );
   }
--- src/components/Lists/List.js
+++ src/components/Lists/List.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import style from './style.css';
 
 const List = ({owner, full_name,description, open_issues_count, stargazers_count}) => (
-  <div className="repository card card-1">
-    <div className="user__avatar">
-        <img src={owner.avatar_url} />
+  <Fragment>
+    <div className="repository card card-1">
+      <div className="user__avatar">
+          <img src={owner.avatar_url} />
+      </div>
+      <div className="repository__info">
+        <h3>{full_name}</h3>
+        <p>{description}</p>
+        <p>
+          <span className="span__bordered">issues: {open_issues_count}</span>
+          <span className="span__bordered">stars: {stargazers_count}</span>
+          Submitted 30 days ago by {owner.login}
+        </p>
+      </div>
     </div>
-    <div className="repository__info">
-      <h3>{full_name}</h3>
-      <p>{description}</p>
-      <p>
-        <span className="span__bordered">issues: {open_issues_count}</span>
-        <span className="span__bordered">stars: {stargazers_count}</span>
-        Submitted 30 days ago by {owner.login}
-      </p>
-    </div>
-  </div>
+  </Fragment>
 );
 
 export default List;
