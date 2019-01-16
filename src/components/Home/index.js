9f9dd95a2e57a56b47099b91d51c6d97383428d8
jaouad <jaouad@nextmedia.ma>
2019-01-16 16:05:18 +0100

    add loading when waiting for data to get received and send it via props to Lists component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 
 import { url } from '../../utils/url';
+import Lists from '../Lists';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      repositories: [],
+      laoding: false
+    }
   }
 
   componentDidMount() {
@@ -13,16 +18,24 @@ class Home extends Component {
   }
 
   fetchApi = () => {
+    this.setState({loading: true})
     axios.get(url)
-      .then(response => console.log(response))
+      .then(({ data }) => this.setState({repositories: data.items, loading: false}))
       .catch(error => console.log(error))
   }
 
 
 
   render() {
+    const { repositories, loading } = this.state;
+    console.log(loading)
     return (
-      <div>jaouad</div>
+      <Fragment>
+        {
+          loading ? <div>loading....</div>
+          : <Lists repositories={repositories} />
+        }
+      </Fragment>
     );
   }
 }
---
+++ src/components/Lists/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Lists = (props) => {
+  console.log(props)
+  return (
+    <div>lists</div>
+  )
+}
+
+export default Lists;
