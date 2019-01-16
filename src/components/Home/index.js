f0b02e90dd408b6ea7ffd223df76ad93dbdec732
jaouad <jaouad@nextmedia.ma>
2019-01-16 22:25:15 +0100

    show end when there is no data to display

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 
 import { url } from '../../utils/url';
 import Lists from '../Lists';
+import style from './style.css'
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       repositories: [],
-      page:0,
+      page:400,
       laoding: false,
       loadingMore: false,
+      end: false,
       url
     }
   }
@@ -27,26 +29,33 @@ class Home extends Component {
       .then((response) => this.setState({repositories: response.data.items, loading: false}))
       .catch(error => {
         console.log(error.response)
-        if(error.response.status === 403) return;
+        if(error.response.status === 403) this.setState({end: true});
       })
   }
 
   fetchMoreData = () => {
     document.addEventListener('scroll',  (event) => {
       if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        this.incrementPage();
-        this.setState({loadingMore: true})
+        if(this.state.end === false) {
+          this.incrementPage();
+          this.setState({loadingMore: true})
+        } else
+          this.setState({loadingMore: false})
         let url = `${this.state.url}&page=${this.state.page}`;
-        axios.get(url)
-          .then(({data}) => {
-            let items = [...this.state.repositories, ...data.items];
-            this.setState({repositories: items, loadingMore: false})
-          })
-          .catch(error => {
-            console.log(error)
-          });
+        this.loadMore(url)
     }
   });
+  }
+
+  loadMore = (url) => {
+    axios.get(url)
+      .then(({data}) => {
+        let items = [...this.state.repositories, ...data.items];
+        this.setState({repositories: items, loadingMore: false})
+      })
+      .catch(error => {
+        this.setState({end: true, loadingMore: false});
+      });
   }
 
   incrementPage = () => {
@@ -60,14 +69,15 @@ class Home extends Component {
 
 
   render() {
-    const { repositories, loading, loadingMore } = this.state;
+    const { repositories, loading, loadingMore, end } = this.state;
     return (
       <Fragment>
         {
           loading ? <div>loading....</div>
           : <Lists repositories={repositories} />
         }
-        {loadingMore && <div>Loading more ....</div>}
+        { loadingMore && <div>Loading more ....</div> }
+        { end && <div className="end">End</div> }
       </Fragment>
     );
   }
